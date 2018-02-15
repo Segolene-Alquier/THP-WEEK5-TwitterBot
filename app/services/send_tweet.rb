b88d0ee0750f11c736bb89308c8665aa76e35a84
Segolene Alquier <segolene.alquier@yahoo.com>
2018-02-15 16:51:44 +0100

    twitter done

--- app/services/send_tweet.rb
+++ app/services/send_tweet.rb
@@ -12,10 +12,10 @@ class SendTweet
 
   def log_in_to_twitter
     @client = Twitter::REST::Client.new do |config|
-        config.consumer_key = ENV["TWITTER_CONSUMER_KEY"]
-        config.consumer_secret = ENV["TWITTER_CONSUMER_SECRET"]
-        config.access_token = ENV["TWITTER_ACCESS_TOKEN"]
-        config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+        config.consumer_key = ENV["TCK"]
+        config.consumer_secret = ENV["TCS"]
+        config.access_token = ENV["TAT"]
+        config.access_token_secret = ENV["TATS"]
     end
   end
 
